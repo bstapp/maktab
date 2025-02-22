50605b7008ff7ddce4166bb3b791df9a869a1a10
bstapp <basitalfat@gmail.com>
2025-02-22 17:30:53 +0700

    up

--- app/views/about/index.php
+++
@@ -1,3 +0,0 @@
-
-  <h1>halaman about</h1>
-  <p>Hallo nama saya <?= $data['nama']?> saya adalah seorang <?= $data['pekerjaan']?> umur saya <?= $data['umur']?> tahun</p>
--- app/views/about/page.php
+++
@@ -1,2 +0,0 @@
-
-  <h1>halaman page</h1>
